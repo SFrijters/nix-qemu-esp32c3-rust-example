08643e20812ed297cae9ae3db0886cc1f814ac5a
Stefan Frijters <sfrijters@gmail.com>
2024-06-12 14:21:10 +0200

    Use upstream blinky

--- blinky/src/main.rs
+++ blinky/src/main.rs
@@ -1,9 +1,23 @@
+// https://github.com/esp-rs/esp-hal/blob/main/examples/src/bin/blinky.rs
+
+//! Blinks an LED
+//!
+//! This assumes that a LED is connected to the pin assigned to `led`. (GPIO0)
+
+//% CHIPS: esp32 esp32c2 esp32c3 esp32c6 esp32h2 esp32s2 esp32s3
+
 #![no_std]
 #![no_main]
 
 use esp_backtrace as _;
-use esp_println::println;
-use esp_hal::{peripherals::Peripherals, system::SystemControl, clock::ClockControl, delay::Delay, gpio::{Io, Level, Output}, entry};
+use esp_hal::{
+    clock::ClockControl,
+    delay::Delay,
+    gpio::{Io, Level, Output},
+    peripherals::Peripherals,
+    prelude::*,
+    system::SystemControl,
+};
 
 #[entry]
 fn main() -> ! {
@@ -11,20 +25,17 @@ fn main() -> ! {
     let system = SystemControl::new(peripherals.SYSTEM);
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
 
-    println!("Hello world!");
-
-    // Set GPIO7 as an output, and set its state high initially.
+    // Set GPIO0 as an output, and set its state high initially.
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
-    let mut led = Output::new(io.pins.gpio7, Level::High);
+    let mut led = Output::new(io.pins.gpio0, Level::High);
 
-    led.set_high();
-
-    // Initialize the Delay peripheral, and use it to toggle the LED state in a
-    // loop.
     let delay = Delay::new(&clocks);
 
     loop {
         led.toggle();
-        delay.delay_millis(500u32);
+        delay.delay_millis(500);
+        led.toggle();
+        // or using `fugit` duration
+        delay.delay(2.secs());
     }
 }
