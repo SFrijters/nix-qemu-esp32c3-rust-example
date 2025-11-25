db9e443b49f3e4e981d010a6fbe775fd1a059d85
Stefan Frijters <sfrijters@gmail.com>
2025-11-25 23:11:20 +0100

    Fix linker issues
    Caused by https://github.com/NixOS/nixpkgs/pull/435278
    
    RUSTFLAGS is now always set and overrides whatever we specify in .cargo/config.toml
    
    TODO: Keep an eye on
    
    https://github.com/NixOS/nixpkgs/pull/464707
    https://github.com/NixOS/nixpkgs/pull/464992
    
    and possibly revert or adjust to the new status quo in nixpkgs.

--- blinky/default.nix
+++ blinky/default.nix
@@ -16,6 +16,27 @@ rustPlatform.buildRustPackage {
     ];
   };
 
+  # Work around https://github.com/NixOS/nixpkgs/pull/435278 - RUSTFLAGS is always set now and overrides .cargo/config.toml.
+  # TODO: Keep an eye on
+  # https://github.com/NixOS/nixpkgs/pull/464707
+  # https://github.com/NixOS/nixpkgs/pull/464992
+  # and possibly revert (move back to .cargo/config.toml) or adjust to the new status quo in nixpkgs.
+  RUSTFLAGS = [
+    "-C"
+    "link-arg=-Tlinkall.x"
+    # Required to obtain backtraces (e.g. when using the "esp-backtrace" crate.)
+    # NOTE: May negatively impact performance of produced code
+    "-C"
+    "force-frame-pointers"
+    # Work around https://github.com/NixOS/nixpkgs/issues/281527 by forcing LLD as the linker:
+    #   >   = note: x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '-flavor'
+    #   >           x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '--as-needed'; did you mean '-mno-needed'?
+    #   >           x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '--gc-sections'; did you mean '--data-sections'?
+    #   >
+    "-C"
+    "linker=rust-lld"
+  ];
+
   cargoLock.lockFile = ./Cargo.lock;
 
   doCheck = false;
