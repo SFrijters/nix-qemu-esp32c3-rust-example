0e6db40281c2ad01d035af4679caec1692ddbbba
Stefan Frijters <sfrijters@gmail.com>
2025-11-25 23:42:11 +0100

    Deduplicate rustflags/RUSTFLAGS

--- blinky/default.nix
+++ blinky/default.nix
@@ -20,22 +20,8 @@ rustPlatform.buildRustPackage {
   # TODO: Keep an eye on
   # https://github.com/NixOS/nixpkgs/pull/464707
   # https://github.com/NixOS/nixpkgs/pull/464992
-  # and possibly revert (move back to .cargo/config.toml) or adjust to the new status quo in nixpkgs.
-  RUSTFLAGS = [
-    "-C"
-    "link-arg=-Tlinkall.x"
-    # Required to obtain backtraces (e.g. when using the "esp-backtrace" crate.)
-    # NOTE: May negatively impact performance of produced code
-    "-C"
-    "force-frame-pointers"
-    # Work around https://github.com/NixOS/nixpkgs/issues/281527 by forcing LLD as the linker:
-    #   >   = note: x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '-flavor'
-    #   >           x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '--as-needed'; did you mean '-mno-needed'?
-    #   >           x86_64-unknown-linux-gnu-gcc: error: unrecognized command-line option '--gc-sections'; did you mean '--data-sections'?
-    #   >
-    "-C"
-    "linker=rust-lld"
-  ];
+  # and possibly revert or adjust to the new status quo in nixpkgs.
+  RUSTFLAGS = (builtins.fromTOML (builtins.readFile ./.cargo/config.toml)).build.rustflags;
 
   cargoLock.lockFile = ./Cargo.lock;
 
